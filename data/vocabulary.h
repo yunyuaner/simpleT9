cc6593bc64c9898de9cab55ace41af576b287e48
Jia Guo <yunyuaner@gmail.com>
2020-03-03 05:49:46 -0800

    update license information

--- data/vocabulary.h
+++ data/vocabulary.h
@@ -1,3 +1,23 @@
+/**
+ *      Copyright (c) 2020 Jia Guo <jiag@ntesec.com.cn>
+ *          
+ *          Shang Hai Hua Yuan Chuang Xin Co., Ltd
+ *                  All Right Reserved
+ * 
+ *  simpleT9 is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 3
+ *  as published by the Free Software Foundation.
+ *   
+ *  simpleT9 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU General Public License
+ *  version 3 along with MediaTomb; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 #ifndef _VOCABULARY_H
 #define _VOCABULARY_H
 
--- ime/main.cpp
+++ ime/main.cpp
@@ -1,11 +1,22 @@
-/*****************************************
- * Copyright (c) All Right Reserved
- * Shang Hai Hua Yuan Chuang Xin Co., Ltd
+/**
+ *      Copyright (c) 2020 Jia Guo <jiag@ntesec.com.cn>
+ *          
+ *          Shang Hai Hua Yuan Chuang Xin Co., Ltd
+ *                  All Right Reserved
  * 
- * Author: Jia Guo <jiag@ntesec.com.cn>
- * Date:   Mar. 3rd 2020
- *
- *****************************************/
+ *  simpleT9 is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 3
+ *  as published by the Free Software Foundation.
+ *   
+ *  simpleT9 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU General Public License
+ *  version 3 along with MediaTomb; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 #include <QApplication>
 #include <QWidget>
 #include <QMainWindow>
