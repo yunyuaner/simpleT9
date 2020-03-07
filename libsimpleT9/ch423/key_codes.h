4503a2db496a3cacf238f83744abdac35fc37cfd
Jia Guo <yunyuaner@gmail.com>
2020-03-07 05:03:28 -0800

    separate libsimpleT9 as a stand-alone library

---
+++ libsimpleT9/ch423/key_codes.h
@@ -0,0 +1,136 @@
+#ifndef _KEY_CODES_H
+#define _KEY_CODES_H
+
+/*
+ * Keys and buttons
+ *
+ * Most of the keys/buttons are modeled after USB HUT 1.12
+ * (see http://www.usb.org/developers/hidpage).
+ * Abbreviations in the comments:
+ * AC - Application Control
+ * AL - Application Launch Button
+ * SC - System Control
+ */
+
+#define KEY_1			2
+#define KEY_2			3
+#define KEY_3			4
+#define KEY_4			5
+#define KEY_5			6
+#define KEY_6			7
+#define KEY_7			8
+#define KEY_8			9
+#define KEY_9			10
+#define KEY_0			11
+#define KEY_F1			59
+#define KEY_F2			60
+#define KEY_F3			61
+#define KEY_F4			62
+#define KEY_F5			63
+#define KEY_F6			64
+#define KEY_F7			65
+#define KEY_F8			66
+#define KEY_F9			67
+#define KEY_F10			68
+#define KEY_Q			16
+#define KEY_W			17
+#define KEY_E			18
+#define KEY_R			19
+#define KEY_T			20
+#define KEY_Y			21
+#define KEY_U			22
+#define KEY_I			23
+#define KEY_O			24
+#define KEY_P			25
+#define KEY_A			30
+#define KEY_S			31
+#define KEY_D			32
+#define KEY_F			33
+#define KEY_G			34
+#define KEY_H			35
+#define KEY_J			36
+#define KEY_K			37
+#define KEY_L			38
+#define KEY_Z			44
+#define KEY_X			45
+#define KEY_C			46
+#define KEY_V			47
+#define KEY_B			48
+#define KEY_N			49
+#define KEY_M			50
+#define KEY_MINUS		12
+#define KEY_DOT			52
+#define KEY_UP			103
+#define KEY_DOWN		108
+#define KEY_LEFT		105
+#define KEY_RIGHT		106
+#define KEY_SPACE		57
+#define KEY_BACKSPACE		14
+#define KEY_ESC			1
+#define KEY_ENTER		28
+#define KEY_PAGEUP		104
+#define KEY_PAGEDOWN		109
+
+/* Value from CH423 via i2c */
+#define KEY_1_CH423			18
+#define KEY_2_CH423			19
+#define KEY_3_CH423			20
+#define KEY_4_CH423			21
+#define KEY_5_CH423			22
+#define KEY_6_CH423			26
+#define KEY_7_CH423			27
+#define KEY_8_CH423			28
+#define KEY_9_CH423			29
+#define KEY_0_CH423			30
+#define KEY_F1_CH423			0
+#define KEY_F2_CH423			1
+#define KEY_F3_CH423			2
+#define KEY_F4_CH423			3
+#define KEY_F5_CH423			4
+#define KEY_F6_CH423			8
+#define KEY_F7_CH423			9
+#define KEY_F8_CH423			10
+#define KEY_F9_CH423			11
+#define KEY_F10_CH423			12
+#define KEY_Q_CH423			81
+#define KEY_W_CH423			88
+#define KEY_E_CH423			53
+#define KEY_R_CH423			82
+#define KEY_T_CH423			84
+#define KEY_Y_CH423			90
+#define KEY_U_CH423			85
+#define KEY_I_CH423			58
+#define KEY_O_CH423			66
+#define KEY_P_CH423			80
+#define KEY_A_CH423			48
+#define KEY_S_CH423			83
+#define KEY_D_CH423			52
+#define KEY_F_CH423			54
+#define KEY_G_CH423			56
+#define KEY_H_CH423			57
+#define KEY_J_CH423			60
+#define KEY_K_CH423			61
+#define KEY_L_CH423			62
+#define KEY_Z_CH423			91
+#define KEY_X_CH423			89
+#define KEY_C_CH423			50
+#define KEY_V_CH423			86
+#define KEY_B_CH423			49
+#define KEY_N_CH423			65
+#define KEY_M_CH423			64
+#define KEY_MINUS_CH423		34
+#define KEY_DOT_CH423			32
+#define KEY_UP_CH423			33
+#define KEY_DOWN_CH423		41
+#define KEY_LEFT_CH423		40
+#define KEY_RIGHT_CH423		42
+#define KEY_SPACE_CH423		43
+#define KEY_BACKSPACE_CH423		35
+#define KEY_ESC_CH423			36
+#define KEY_ENTER_CH423		44
+#define KEY_PAGEUP_CH423		17
+#define KEY_PAGEDOWN_CH423		25
+
+#define KEY_LEFTSHIFT		42
+
+#endif /* _KEY_CODES_H */
